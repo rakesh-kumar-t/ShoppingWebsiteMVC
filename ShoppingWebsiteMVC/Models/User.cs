d097ccbdba782703f573944521727f5f4843b9e6
Swathi Madhavan <swathimadhavan363@gmail.com>
2020-09-06 20:41:49 +0530

    Validations added

--- ShoppingWebsiteMVC/Models/User.cs
+++ ShoppingWebsiteMVC/Models/User.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ShoppingWebsiteMVC.Models
 {
@@ -19,10 +20,13 @@ namespace ShoppingWebsiteMVC.Models
         [Required]
         [Display(Name = "Lastname")]
         public string Lastname { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Password is required")]
         [Display(Name = "Password")]
         [DataType(DataType.Password)]
+        [StringLength(255,ErrorMessage ="Password should be minimum of 8 characters",MinimumLength =8)]
+        [RegularExpression("^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])|(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^a-zA-Z0-9])|(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])).{8,}$", ErrorMessage = "Passwords must be at least 8 characters and should contain : upper case (A-Z), lower case (a-z), number (0-9) and special character (e.g. !@#$%^&*)")]
         public string Password { get; set; }
+        
         [NotMapped]
         [Compare("Password")]
         [DataType(DataType.Password)]
@@ -33,6 +37,9 @@ namespace ShoppingWebsiteMVC.Models
         public string Address { get; set; }
         [Display(Name = "ContactNumber")]
         [Required(ErrorMessage = "Contact number is required")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Your mobile number  is not valid")]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Please enter mobile number")]
+
         public string ContactNumber { get; set; }
         [Display(Name = "City")]
         [Required(ErrorMessage = "City is required")]
