9858d21dc5b41ca272457da407fb8d54b9ebd7d7
Sreelakshmi C <sreelakshmichelat@gmail.com>
2020-09-06 18:43:23 +0530

    added Transaction and Transaction Context.

---
+++ ShoppingWebsiteMVC/Models/TransactionContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ShoppingWebsiteMVC.Models
+{
+    public class TransactionContext:DbContext
+    {
+        public TransactionContext()
+        {
+
+        }
+        public DbSet<Transaction> Transactions { get; set; }
+
+    }
+}
