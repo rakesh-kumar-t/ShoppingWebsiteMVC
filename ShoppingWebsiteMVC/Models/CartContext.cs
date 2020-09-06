d8fc22ace920656574ebe88c8a10ce70abf084c1
Sreelakshmi C <sreelakshmichelat@gmail.com>
2020-09-06 20:15:48 +0530

    added cart and cartcontext

---
+++ ShoppingWebsiteMVC/Models/CartContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ShoppingWebsiteMVC.Models
+{
+    public class CartContext:DbContext
+    {
+        public CartContext()
+        {
+
+        }
+        public DbSet<Cart> Carts { get; set; }
+    }
+}
