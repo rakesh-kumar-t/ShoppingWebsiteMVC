6f1e4c3ef994c0a30e20f7b45d51202cb5fef63a
Swathi Madhavan <swathimadhavan363@gmail.com>
2020-09-06 18:27:01 +0530

    Created class and context  class for product

---
+++ ShoppingWebsiteMVC/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingWebsiteMVC.Models
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        [Display(Name ="Product Code")]
+        public int ProductId { get; set; }
+        [Required]
+        [Display(Name ="Product Name")]
+        public string ProductName { get; set; }
+        [Required]
+        [Display(Name ="Brand")]
+        public string CategoryName { get; set; }
+        [Required]
+        [Display(Name ="Units")]
+        public int Units { get; set; }
+        [Display(Name ="Discount")]
+        public double Discount { get; set; }
+        [Required]
+        [Display(Name ="Supplier Name")]
+        public string SupplierName { get; set; }
+
+    }
+}
