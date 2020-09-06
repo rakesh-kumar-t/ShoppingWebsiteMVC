220298fcbe139cf5eaea42380d0b7c7d8acb0dc4
Swathi Madhavan <swathimadhavan363@gmail.com>
2020-09-06 20:06:56 +0530

    Created Feedback class and context class

---
+++ ShoppingWebsiteMVC/Models/Feedback.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingWebsiteMVC.Models
+{
+    public class Feedback
+    {
+        [Key]
+        [Required]
+        public string ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public string FeedBack { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserId { get; set; }
+
+    }
+}
---
+++ ShoppingWebsiteMVC/Models/FeedbackContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingWebsiteMVC.Models
+{
+    public class FeedbackContext:DbContext
+    {
+        public FeedbackContext()
+        {
+
+        }
+        public DbSet<Feedback> Feedbacks { get; set; }
+    }
+}
