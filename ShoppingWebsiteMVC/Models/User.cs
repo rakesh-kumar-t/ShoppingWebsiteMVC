ada28b68ee25e018469c2b162b48910349b0db0d
Rakesh <rakeshkumart2760@gmail.com>
2020-09-06 17:58:58 +0530

    userclass created

--- ShoppingWebsiteMVC/Models/User.cs
+++ ShoppingWebsiteMVC/Models/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,39 @@ namespace ShoppingWebsiteMVC.Models
 {
     public class User
     {
+        [Key]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string UserId { get; set; }
+        [Required]
+        [Display(Name = "Firstname")]
+        public string Firstname { get; set; }
+        [Required]
+        [Display(Name = "Lastname")]
+        public string Lastname { get; set; }
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [NotMapped]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Confirm Password required")]
+        public string ConfirmPassword { get; set; }
+        [Display(Name = "Address")]
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+        [Display(Name = "ContactNumber")]
+        [Required(ErrorMessage = "Contact number is required")]
+        public string ContactNumber { get; set; }
+        [Display(Name = "City")]
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; }
+        [Display(Name = "Country")]
+        [Required(ErrorMessage = "Country is required")]
+        public string Country { get; set; }
+        [Required]
+        [Display(Name = "Role")]
+        public string Role { get; set; }
     }
 }
