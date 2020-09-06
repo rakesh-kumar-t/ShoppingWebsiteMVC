73e64abd1d76e2436ede39e780a065c956fcea9d
Swathi Madhavan <swathimadhavan363@gmail.com>
2020-09-06 18:39:46 +0530

    Added Price in Product Class

--- ShoppingWebsiteMVC/Models/Product.cs
+++ ShoppingWebsiteMVC/Models/Product.cs
@@ -10,21 +10,25 @@ namespace ShoppingWebsiteMVC.Models
     {
         [Key]
         [Required]
-        [Display(Name ="Product Code")]
+        [Display(Name ="ProductCode")]
         public int ProductId { get; set; }
         [Required]
-        [Display(Name ="Product Name")]
+        [Display(Name ="ProductName")]
         public string ProductName { get; set; }
         [Required]
         [Display(Name ="Brand")]
         public string CategoryName { get; set; }
         [Required]
+        [Display(Name ="Cost")]
+        public double Price { get; set; }
+        [Required]
         [Display(Name ="Units")]
+
         public int Units { get; set; }
         [Display(Name ="Discount")]
         public double Discount { get; set; }
         [Required]
-        [Display(Name ="Supplier Name")]
+        [Display(Name ="SupplierName")]
         public string SupplierName { get; set; }
 
     }
