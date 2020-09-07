7879f1c2af6d68bd04de31021fd64dab4c0d3f2d
Sreelakshmi C <sreelakshmichelat@gmail.com>
2020-09-08 01:00:33 +0530

    added AdminProduct Controller.

---
+++ ShoppingWebsiteMVC/Controllers/AdminproductController.cs
@@ -0,0 +1,115 @@
+﻿using ShoppingWebsiteMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ShoppingWebsiteMVC.Controllers
+{
+    public class AdminproductController : Controller
+    {
+        // GET: Adminproduct
+        ProductContext dbo = new ProductContext();
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View(dbo.Products.ToList());
+        }
+        [Authorize]
+        public ActionResult Details(int? ProductId)
+        {
+            if (ProductId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = dbo.Products.Find(ProductId);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductId,ProductName,CategoryName,Price,Discount,SupplierName")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                dbo.Products.Add(product);
+                dbo.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+        [Authorize]
+        public ActionResult Edit(int? ProductId)
+        {
+            if (ProductId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = dbo.Products.Find(ProductId);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductId,ProductName,CategoryName,Price,Discount,SupplierName")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                dbo.Entry(product).State = EntityState.Modified;
+                dbo.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        [Authorize]
+        public ActionResult Delete(int? ProductId)
+        {
+            if (ProductId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = dbo.Products.Find(ProductId);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int ProductId)
+        {
+            Product product = dbo.Products.Find(ProductId);
+            dbo.Products.Remove(product);
+            dbo.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                dbo.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
