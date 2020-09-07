08bdc6d86d4717f1c21659723791140623e6e10c
Rakesh <rakeshkumart2760@gmail.com>
2020-09-07 23:28:08 +0530

    added product view by category

--- ShoppingWebsiteMVC/Controllers/ProductController.cs
+++ ShoppingWebsiteMVC/Controllers/ProductController.cs
@@ -16,6 +16,33 @@ namespace ShoppingWebsiteMVC.Controllers
         {
             return View();
         }
+        // GET : Products by Category
+        [Authorize]
+        [HttpGet]
+        public ActionResult Index(string category)
+        {
+            using(ProductContext db=new ProductContext())
+            {
+                category = category.ToLower();
+                if (category == "all")
+                {
+                    return View(db.Products.ToList());
+                }
+                else
+                {
+                    var products = db.Products.Where(p => p.CategoryName.Equals(category)).FirstOrDefault();
+                    if (products != null)
+                    {
+                        return View(products);
+                    }
+                    else
+                    {
+                        ViewBag.Error = "Invalid Category";
+                    }
+                    return View("Index");
+                }
+            }
+        }
 
     }
 }
