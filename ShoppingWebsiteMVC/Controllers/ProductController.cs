bc486e77fc72b838d15e0f6ab45f24a2302c5821
Sreelakshmi C <sreelakshmichelat@gmail.com>
2020-09-07 21:02:39 +0530

    product controller

---
+++ ShoppingWebsiteMVC/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingWebsiteMVC.Models;
+
+
+namespace ShoppingWebsiteMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
