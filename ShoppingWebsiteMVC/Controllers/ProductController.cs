77b3cf207561a77604f9bcc7565010c63a1bca68
Swathi Madhavan <70809762+swathi363@users.noreply.github.com>
2020-09-08 20:26:46 +0530

    Search added in product controller

--- ShoppingWebsiteMVC/Controllers/ProductController.cs
+++ ShoppingWebsiteMVC/Controllers/ProductController.cs
@@ -43,6 +43,30 @@ namespace ShoppingWebsiteMVC.Controllers
                 }
             }
         }
+        public ActionResult Search(string ProductName)
+        {
+            List<Product> list = new List<Product>();
+            using(ProductContext db=new ProductContext())
+            {
+                if(String.IsNullOrEmpty(ProductName))
+
+                {
+                    var query = from product in db.Products select new Product { ProductId = product.ProductId, ProductName = product.ProductName, CategoryName = product.CategoryName, Price = product.Price,Units=product.Units,Discount = product.Discount, SupplierName = product.SupplierName };
+                    list = query.ToList();
+
+                }
+                else
+                {
+                    var query = from product in db.Products
+                                where product.ProductName.ToLower().Contains(ProductName.ToLower())
+                                select new Product { ProductId = product.ProductId, ProductName = product.ProductName, CategoryName = product.CategoryName, Price = product.Price, Units = product.Units, Discount = product.Discount, SupplierName = product.SupplierName };
+                    list = query.ToList();
+
+                }
+                ViewBag.Title= "Search Result";
+                return View("List", "list");
+            }
+        }
 
 
     }
