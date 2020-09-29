e34684dffdffbc881eacd2e9f8b6d22279a50ac5
Swathi Madhavan <swathimadhavan363@gmail.com>
2020-09-29 13:28:44 +0530

    change

--- ShoppingWebsiteMVC/Models/ShoppingContext.cs
+++ ShoppingWebsiteMVC/Models/ShoppingContext.cs
@@ -8,7 +8,7 @@ namespace ShoppingWebsiteMVC.Models
 {
     public class ShoppingContext:DbContext
     {
-        public ShoppingContext():base("Name=Dbconfig") 
+        public ShoppingContext()
         {
 
         }
