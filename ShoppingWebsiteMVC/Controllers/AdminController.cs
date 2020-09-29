fc42d643897de7fc115f7906f03be01ea26c2baf
Swathi Madhavan <swathimadhavan363@gmail.com>
2020-09-29 19:54:14 +0530

    bind

--- ShoppingWebsiteMVC/Controllers/AdminController.cs
+++ ShoppingWebsiteMVC/Controllers/AdminController.cs
@@ -68,7 +68,7 @@ namespace ShoppingWebsiteMVC.Controllers
         [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ProductId,ProductName,CategoryName,BrandName,Price,Units,Discount,SupplierName")] Product product,HttpPostedFileBase Proimage)
+        public ActionResult Create([Bind(Include = "ProductName,CategoryName,BrandName,Price,Units,Discount,SupplierName")] Product product,HttpPostedFileBase Proimage)
         {
             ViewBag.SubCategory = db.SubCategories.ToList();
             ViewBag.Supplier = db.Suppliers.ToList();
