64e05e514e2d65ad7d4650114c30cc2ca35e2e16
Sreelakshmi C <sreelakshmichelat@gmail.com>
2020-09-29 20:07:23 +0530

    migration files

---
+++ ShoppingWebsiteMVC/Migrations/202009291434356_db change.cs
@@ -0,0 +1,16 @@
+﻿namespace ShoppingWebsiteMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class dbchange : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ ShoppingWebsiteMVC/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace ShoppingWebsiteMVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ShoppingWebsiteMVC.Models.ShoppingBagContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "ShoppingWebsiteMVC.Models.ShoppingBagContext";
+        }
+
+        protected override void Seed(ShoppingWebsiteMVC.Models.ShoppingBagContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
